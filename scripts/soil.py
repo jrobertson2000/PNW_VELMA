2c77c1f29eafa550a6c1f0f0049cb7e6e64a1b05
ianpdavies <yon.davies@gmail.com>
2020-10-26 15:59:29 -0700

    can't reclassify using remap (might be arcpy bug) so using a hacky workaround of copying Python snippet from ArcGIS

--- scripts/soil.py
+++ scripts/soil.py
@@ -22,6 +22,8 @@ arcpy.env.overwriteOutput = True
 
 # Create temp directory for intermediary files
 temp_dir = tempfile.mkdtemp()
+temp_gdb = 'temp.gdb'
+arcpy.CreateFileGDB_management(temp_dir, temp_gdb)
 
 # =======================================================================
 # Get soil texture classes
@@ -73,63 +75,69 @@ create_texture_map(gssurgo)
 statsgo2 = str(config.statsgo2)
 create_texture_map(statsgo2)
 
-# Merge soil texture subclasses into classes, assign integer IDs
-fields = ['texcl']
-soil_out = str(config.gssurgo)
-arcpy.MakeRasterLayer_management(in_raster=soil_out, out_rasterlayer='soil_out', band_index="1")
-cursor = arcpy.da.SearchCursor('soil_out', fields)
-df1 = pd.DataFrame(data=np.unique([row[0] for row in cursor]), columns=['texture'])
+# # Merge soil texture subclasses into classes, assign integer IDs
+# fields = ['texcl']
+# soil_out = str(config.gssurgo)
+# arcpy.MakeRasterLayer_management(in_raster=soil_out, out_rasterlayer='soil_out', band_index="1")
+# cursor = arcpy.da.SearchCursor('soil_out', fields)
+# df1 = pd.DataFrame(data=np.unique([row[0] for row in cursor]), columns=['texture'])
+#
+# fields = ['TEXCL']
+# soil_out = str(config.statsgo2)
+# arcpy.MakeRasterLayer_management(in_raster=soil_out, out_rasterlayer='soil_out', band_index="1")
+# cursor = arcpy.da.SearchCursor('soil_out', fields)
+# df2 = pd.DataFrame(data=np.unique([row[0] for row in cursor]), columns=['texture'])
+#
+# unique_classes = pd.concat([df1['texture'], df2['texture']]).unique()
+# df = pd.DataFrame(data=unique_classes, columns=['texture'])
+#
+# # Create soil texture ID map
+# subclasses = {('Loamy sand', 'Loamy coarse sand', 'Loamy fine sand', 'Loamy very fine sand'): 1,
+#               ('Sandy loam', 'Coarse sandy loam', 'Very fine sandy loam', 'Fine sandy loam'): 2,
+#               ('Sand', 'Fine sand', 'Sand', 'Very fine sand', 'Sand', 'Coarse sand'): 3,
+#               ('Silt', 'Silt loam'): 4}
+#
+# subclass_map = {}
+# for k, v in subclasses.items():
+#     for key in k:
+#         subclass_map[key] = v
+#
+# # subclass_map['None'] = 0
+# subclass_map['Sandy clay loam'] = 5
+# subclass_map['Clay loam'] = 6
+# subclass_map['Clay'] = 7
+# subclass_map['Loam'] = 8
+# subclass_map['Silty clay'] = 9
+# subclass_map['Silty clay loam'] = 10
+# subclass_map['Sandy clay'] = 11
 
-fields = ['TEXCL']
-soil_out = str(config.statsgo2)
-arcpy.MakeRasterLayer_management(in_raster=soil_out, out_rasterlayer='soil_out', band_index="1")
-cursor = arcpy.da.SearchCursor('soil_out', fields)
-df2 = pd.DataFrame(data=np.unique([row[0] for row in cursor]), columns=['texture'])
+# # # Remap
+# remap_values = zip(subclass_map.keys(), subclass_map.values())
+# remap_values = arcpy.sa.RemapValue(remap_values)
+#
+# # gssurgo_temp = temp_dir + '/gssurgo.tif'
+# arcpy.CopyRaster_management(in_raster=str(config.gssurgo_out), out_rasterdataset=gssurgo_temp)
+# for field in arcpy.ListFields(gssurgo):
+#     print field.type
 
-unique_classes = pd.concat([df1['texture'], df2['texture']]).unique()
-df = pd.DataFrame(data=unique_classes, columns=['texture'])
-
-# Create soil texture ID map
-subclasses = {('Loamy sand', 'Loamy coarse sand', 'Loamy fine sand', 'Loamy very fine sand'): 1,
-              ('Sandy loam', 'Coarse sandy loam', 'Very fine sandy loam', 'Fine sandy loam'): 2,
-              ('Sand', 'Fine sand', 'Sand', 'Very fine sand', 'Sand', 'Coarse sand'): 3,
-              ('Silt', 'Silt loam'): 4}
-
-subclass_map = {}
-for k, v in subclasses.items():
-    for key in k:
-        subclass_map[key] = v
-
-# subclass_map['None'] = 0
-subclass_map['Sandy clay loam'] = 5
-subclass_map['Clay loam'] = 6
-subclass_map['Clay'] = 7
-subclass_map['Loam'] = 8
-subclass_map['Silty clay'] = 9
-subclass_map['Silty clay loam'] = 10
-subclass_map['Sandy clay'] = 11
-
-# Remap
-remap_values = zip(subclass_map.keys(), subclass_map.values())
-remap_values = arcpy.sa.RemapValue(remap_values)
-
-# gssurgo_temp = temp_dir + '/gssurgo.tif'
-# arcpy.CopyRaster_management(in_raster=gssurgo, out_rasterdataset=gssurgo_temp)
-# for field in arcpy.ListFields(gssurgo_temp):
-#     print field.name
-
-# soil_remap = arcpy.sa.Reclassify(in_raster=gssurgo_temp, reclass_field='texcl', remap=remap_values)  # This doesn't work but it should
+# soil_remap = arcpy.sa.Reclassify(in_raster=gssurgo, reclass_field='texcl', remap=remap_values)  # This doesn't work but it should
 # soil_remap = arcpy.sa.Reclassify(in_raster=statsgo2, reclass_field='TEXCL', remap=remap_values)
 
-# The following inputs are layers or table views: "statsgo2_text"
-# If the above works, was going to use soil_remap to reshape and convert to ASCII
-# Instead have to reclassify in ArcMap to new files, then continue from there
-# Manually add the file paths for gssurgo_reclass/statsgo_reclass below after creation in ArcMap
-gssurgo_reclass = 'C:/Users/ipdavies/Documents/ArcGIS/Default.gdb/Reclass_Mapu2'
-statsgo2_reclass = 'C:/Users/ipdavies/Documents/ArcGIS/Default.gdb/Reclass_Mapu3'
+# This is a hacky solution - copied Python snippet from running reclassify in ArcMap
+remap_values = "'Silt loam' 4;'Sandy loam' 2;'Fine sandy loam' 2;Loam 8;'Coarse sandy loam' 2;'Loamy sand' 1;Sand 3;'Loamy fine sand' 1;'Sandy clay loam' 5;'Very fine sandy loam' 2;'Coarse sand' 3;'Clay loam' 6;'Loamy coarse sand' 1;'Fine sand' 3;'Silty clay loam' 10;'Silty clay' 9;Clay 7;'Very fine sand' 3;Silt 4;'Loamy very fine sand' 1"
+gssurgo_reclass = temp_dir + '/temp.gdb/gssurgo_reclass'
+arcpy.gp.Reclassify_sa(gssurgo, "texcl", remap_values, gssurgo_reclass, "DATA")
+
+statsgo2_reclass = temp_dir + '/temp.gdb/statsgo2_reclass'
+arcpy.gp.Reclassify_sa(statsgo2, 'TEXCL', remap_values, statsgo2_reclass, "DATA")
 
 reshape(gssurgo_reclass, config.gssurgo_out, 'gSSURGO', 'NEAREST', dem_specs, temp_dir, roi_layers)
 reshape(statsgo2_reclass, config.statsgo2_out, 'STATSGO2', 'NEAREST', dem_specs, temp_dir, roi_layers)
+
+arcpy.Delete_management(gssurgo_reclass)
+arcpy.Delete_management(statsgo2_reclass)
+del gssurgo_reclass
+del statsgo2_reclass
 
 gssurgo_temp = temp_dir + '/gssurgo.asc'
 statsgo2_temp = temp_dir + '/statsgo2.asc'
@@ -139,3 +147,8 @@ arcpy.RasterToASCII_conversion(in_raster=str(config.statsgo2_out), out_ascii_fil
 
 # Merge gSSURGO and STATSGO2 together
 mergeSoils(gssurgo_temp, statsgo2_temp, str(config.soil_velma))
+
+arcpy.Delete_management(gssurgo_temp)
+arcpy.Delete_management(statsgo2_temp)
+
+
