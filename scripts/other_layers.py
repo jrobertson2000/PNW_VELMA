4cf881b517b67faa661dcfa480b8d33951d48da9
ianpdavies <yon.davies@gmail.com>
2020-10-26 15:57:59 -0700

    formatting of all other layers to DEM extent/proj that don't require special treatment

--- scripts/other_layers.py
+++ scripts/other_layers.py
@@ -5,8 +5,8 @@ import arcpy
 from arcpy import env
 import tempfile
 import config as config
-import numpy as np
 import imp
+from utils import reshape, getDEMspecs, getROI
 imp.reload(config)
 arcpy.CheckOutExtension('Spatial')
 # ======================================================================================================================
@@ -16,8 +16,6 @@ arcpy.env.overwriteOutput = True
 
 # Create temp directory for intermediary files
 temp_dir = tempfile.mkdtemp()
-temp_gdb = 'temp.gdb'
-arcpy.CreateFileGDB_management(temp_dir, temp_gdb)
 
 # =======================================================================
 # Mosaic rasters
@@ -54,51 +52,6 @@ except AttributeError:
     pass
 
 # =======================================================================
-# Set up functions
-# =======================================================================
-
-class getROI:
-    def __init__(self, roi, dem_in):
-        self.roi = roi
-        self.roi_buff = temp_dir + '/roi_buff.shp'
-        self.roi_reproj = temp_dir + '/roi_reproj.shp'
-        
-        arcpy.Project_management(in_dataset=roi, out_dataset=self.roi_reproj, out_coor_system=arcpy.Describe(dem_in).spatialReference)
-        arcpy.CopyFeatures_management(self.roi_reproj, roi)
-        arcpy.Buffer_analysis(in_features=roi, out_feature_class=self.roi_buff,
-                              buffer_distance_or_field="15 Kilometers",
-                              line_side="FULL", line_end_type="ROUND", dissolve_option="NONE", dissolve_field="",
-                              method="PLANAR")
-        
-
-
-class getDEMspecs:
-    """ Get DEM cell size and spatial ref for reprojection/resampling of rasters """
-    def __init__(self, dem_out):
-        self.dem = dem_out
-        self.x = arcpy.GetRasterProperties_management(dem_out, 'CELLSIZEX').getOutput(0)
-        self.y = arcpy.GetRasterProperties_management(dem_out, 'CELLSIZEY').getOutput(0)
-        self.xy = self.x + ' ' + self.y
-        self.spatial_ref = arcpy.Describe(dem_out).spatialReference
-        self.extent = arcpy.sa.Raster(dem_out).extent
-
-
-def reshape(in_path, out_path, name, resamp_type, dem_specs, temp_dir, roi_layers):
-    print 'Prepping ' + name + ' ...'
-    file_in = str(in_path)
-    buff = temp_dir + '/buff.tif'
-    reproj = temp_dir + '/reproj.tif'
-    file_out = str(out_path)
-    arcpy.Clip_management(in_raster=file_in, out_raster=buff, in_template_dataset=roi_layers.roi_buff,
-                          clipping_geometry=True)
-    arcpy.ProjectRaster_management(in_raster=buff, out_raster=reproj, out_coor_system=dem_specs.spatial_ref,
-                                   resampling_type=resamp_type,
-                                   cell_size=dem_specs.xy)
-    envelope = '{} {} {} {}'.format(dem_specs.extent.XMin, dem_specs.extent.YMin, dem_specs.extent.XMax, dem_specs.extent.YMax)
-    arcpy.Clip_management(in_raster=reproj, out_raster=file_out, rectangle=envelope, nodata_value=-9999)
-    print'Done'
-
-# =======================================================================
 # Clip to study area buffer, reproject, resample rasters
 # =======================================================================
 
@@ -108,20 +61,14 @@ try:
 except NameError:
     dem_in = str(config.dem)
 
-dem_out = str(config.dem_out)
-dem_spatialref = arcpy.Describe(dem_in).spatialReference
-
 roi = str(config.study_area)
-roi_layers = getROI(roi, dem_in)
-arcpy.Clip_management(in_raster=dem_in, out_raster=dem_out, in_template_dataset=roi_layers.roi, nodata_value=-9999, clipping_geometry=True)
+dem_in = str(config.dem)
+dem_out = str(config.dem_out)
+roi_layers = getROI(roi, dem_in, temp_dir)
 
+arcpy.Clip_management(in_raster=dem_in, out_raster=dem_out, in_template_dataset=roi_layers.roi, nodata_value=-9999,
+                      clipping_geometry=True)
 dem_specs = getDEMspecs(dem_out)
-
-# gSSURGO map
-reshape(config.gssurgo, config.gssurgo_out, 'gSSURGO', 'NEAREST', dem_specs, temp_dir, roi_layers)
-
-# STATSGO2 map
-reshape(config.statsgo2, config.statsgo2_out, 'STATSGO2', 'NEAREST', dem_specs, temp_dir, roi_layers)
 
 # # NLCD
 # reshape(config.nlcd, config.nlcd_out, 'nlcd', 'NEAREST', temp_dir, roi_layers)
@@ -152,9 +99,5 @@ reshape(config.cover_id, config.cover_id_out, 'cover_id', 'NEAREST', dem_specs, 
 # Cover age
 reshape(config.cover_age, config.cover_age_out, 'cover_age', 'NEAREST', dem_specs, temp_dir, roi_layers)
 
-# Heat load index: calculate for study area
-arcpy.ImportToolbox(str(config.geomorph_tbx_path))
-arcpy.hli(Select_DEM=dem_out, HLI_Output_Raster="C:/Users/ipdavies/tnc_velma/data/topography/ellsworth/hli_ells.tif")
-
-
-
+# Cover type
+reshape(config.cover_type, config.cover_type_out, 'cover_type', 'NEAREST', dem_specs, temp_dir, roi_layers)
