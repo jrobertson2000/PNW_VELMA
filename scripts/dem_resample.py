d5c0ab2832cce0b93c1730a5d176581afcbe201d
ianpdavies <yon.davies@gmail.com>
2020-11-02 12:13:30 -0800

    Fixed bug where output DEM had NoData cells/misaligned clipping

--- scripts/dem_resample.py
+++ scripts/dem_resample.py
@@ -12,6 +12,7 @@ import config as config
 from pathlib import Path
 import numpy as np
 import imp
+from utils import getROI
 imp.reload(config)
 
 # ======================================================================================================================
@@ -26,10 +27,15 @@ temp_dir = tempfile.mkdtemp()
 # Resample Ellsworth DEM from 1m to 10m, then mosaic with DEM data outside of study area
 # =======================================================================================
 
-# Would be more dynamic to open the study area and get extent from that, in case DEM isn't already clipped
-
 dem_raw = str(config.dem_raw)
 dem_resamp = temp_dir + '/dem_resamp.tif'
+dem_resamp_clip = temp_dir + '/dem_resamp_clip.tif'
+
+# Get extent of study area
+study_area = str(config.study_area)
+roi = getROI(study_area, dem_raw, temp_dir)
+extent = arcpy.Describe(roi.roi).extent
+envelope = '{} {} {} {}'.format(extent.XMin, extent.YMin, extent.XMax, extent.YMax)
 
 # Resample study area DEM
 arcpy.Resample_management(in_raster=dem_raw, out_raster=dem_resamp, cell_size="10 10", resampling_type="CUBIC")
@@ -39,22 +45,25 @@ dem_border_proj = temp_dir + '/dem_border_proj.tif'
 dem_border_clip = temp_dir + '/dem_border_clip.tif'
 dem = str(config.dem)  # Final output DEM
 
-# Get cell size and minimum bounding box of study area DEM
+# Get cell size of DEM
 x = arcpy.GetRasterProperties_management(dem_resamp, 'CELLSIZEX').getOutput(0)
 y = arcpy.GetRasterProperties_management(dem_resamp, 'CELLSIZEY').getOutput(0)
 xy = '{} {}'.format(x, y)
 proj = arcpy.Describe(dem_resamp).spatialReference
-extent = arcpy.sa.Raster(dem_resamp).extent
-envelope = '{} {} {} {}'.format(extent.XMin, extent.YMin, extent.XMax, extent.YMax)
+
+# Clip base DEM to study area
+arcpy.Clip_management(in_raster=dem_resamp, out_raster=dem_resamp_clip, rectangle=envelope, nodata_value=-9999)
 
 # Reproject, clip, and mosaic border DEM
 arcpy.ProjectRaster_management(in_raster=dem_border, out_raster=dem_border_proj, out_coor_system=proj,
                                resampling_type='CUBIC',
                                cell_size=xy)
 
+arcpy.Mosaic_management([dem_resamp_clip, dem_border_proj], target=dem_border_proj, mosaic_type='FIRST')
+
 arcpy.Clip_management(in_raster=dem_border_proj, out_raster=dem_border_clip, rectangle=envelope, nodata_value=-9999)
 
-arcpy.Mosaic_management([dem_resamp, dem_border_clip], target=dem_border_clip, mosaic_type='FIRST')
-
 arcpy.RasterToASCII_conversion(in_raster=dem_border_clip, out_ascii_file=dem)
+
+
 
