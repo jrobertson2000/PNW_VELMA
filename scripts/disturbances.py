73d4e5cfe93a962e8164b788473a1190e29023de
ianpdavies <yon.davies@gmail.com>
2020-12-16 17:29:03 -0800

    Exporting binary filter maps instead of a CSV with filter IDs

--- scripts/disturbances.py
+++ scripts/disturbances.py
@@ -1,33 +1,23 @@
-# Creates CSV with filter map IDs of stands to be affected by a given disturbance
+# Creates binary filter maps of stands to be affected by a given disturbance
+# In VELMA, for each disturbance (e.g. clearcut, 1st trimming, 2nd trimming) the user specifies an integer filter map
+# and the integers which will be affected by a disturbance. The exported filter maps from this script have 1 for pixels
+# included in a disturbance, 0 for those excluded. User then specifies 1 for 'initializeFilterIds' in disturbance
+# parameters.
 # Script written in Python 3.7
 
 import config as config
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 import tempfile
-import importlib
-import pyproj
-from utils import flowlines
-from scipy import ndimage
-
-importlib.reload(config)
+from soil_merger import readHeader
 # ======================================================================================================================
-
 # Create temp directory for intermediary files
 temp_dir = tempfile.mkdtemp()
 
-# =======================================================================
-# For each disturbance (e.g. clearcut, 1st trimming, 2nd trimming) the user specifies an integer filter map and the
-# integers which will be affected by a disturbance. Each stand in the filter map has a unique value, with a 30ft no
-# management buffer around streams with a value of 0.
-#
-# This script exports a CSV with the integers that are selected for each disturbance, which can be entered into the
-# VELMA GUI.
-# =======================================================================
-
-filter_map_path = str(config.cover_id_velma.parents[0] / 'filtermap.asc')
-filter_map = np.loadtxt(filter_map_path, skiprows=6)
+# cover_id is a map of cover_ids created in cover_rasterize_stands.py corresponding to stands, no management buffer (0),
+# and water/area outside of watershed (1)
+cover_id_path = str(config.cover_id_velma.parents[0] / 'filtermap.asc')
+cover_id = np.loadtxt(cover_id_path, skiprows=6)
 # These are the VELMA_IDs of the Ellsworth Experimental Basins
 basin_ids = pd.read_csv(str(config.cover_id_velma.parents[0] / 'experimental_basin_velma_id.csv'))
 
@@ -37,28 +27,39 @@ try:
 except FileExistsError:
     pass
 
-stands = [int(x) for x in np.unique(filter_map)]
-
 # =======================================================================
 # Disturbances
-disturbance_stands = []
-disturbances = []
+
+# Convert filter_map to list, then loop through and convert pixels to 1 if in stands_include, 0 if excluded
+cover_id_list = cover_id.flatten().tolist()
+stands = [int(x) for x in np.unique(cover_id)]
+header = readHeader(cover_id_path)
+
+
+def create_filter_map(disturbance_name, exclude, include):
+    filter_map_list = [1 if x in stands_include else 0 for x in cover_id_list]
+    filter_map = np.array(filter_map_list).reshape(cover_id.shape)
+    outfile = filter_dir / '{}.asc'.format(disturbance_name)
+    f = open(outfile, 'w')
+    f.write(header)
+    np.savetxt(f, filter_map, fmt='%i')
+    f.close()
+    return filter_map
+
 
 # ===================================
 disturbance = 'industrial_clearcut'
 # All stands can be cut
-stands_exclude = [0, 1]  # Exclude no mgmt buffer and water/area outside watershed
+stands_exclude = [0, 1]  # Exclude no mgmt buffer (0) and water/area outside watershed (1)
 stands_include = [x for x in stands if x not in stands_exclude]
-disturbance_stands.append(stands_include)
-disturbances.append(disturbance)
+map1 = create_filter_map(disturbance, stands_exclude, stands_include)
 
 # ===================================
 disturbance = 'active_all'
 # All stands can be cut
 stands_exclude = [0, 1]  # Exclude no mgmt buffer and water/area outside watershed
 stands_include = [x for x in stands if x not in stands_exclude]
-disturbance_stands.append(stands_include)
-disturbances.append(disturbance)
+map2 = create_filter_map(disturbance, stands_exclude, stands_include)
 
 # ===================================
 disturbance = 'current'
@@ -67,32 +68,4 @@ stands_exclude = basin_ids.loc[(basin_ids['mgmt'] == 'passive') | (basin_ids['mg
 stands_exclude.append(0)
 stands_exclude.append(1)
 stands_include = [x for x in stands if x not in stands_exclude]
-disturbance_stands.append(stands_include)
-disturbances.append(disturbance)
-
-# Combine all the disturbances together
-df = pd.DataFrame({'disturbance': disturbances, 'ids': disturbance_stands})
-
-# Export
-df['ids'] = df['ids'].astype('string')
-df['ids'] = df['ids'].map(lambda x: x.lstrip('[]').rstrip('aAbBcC'))
-df.to_csv(str(filter_dir / 'disturbance_filter_ids.csv'), index=False)
-
-
-# # Create list of stands that will be excluded from disturbance
-stands_exclude = basin_ids.loc[(basin_ids['mgmt'] == 'passive') | (basin_ids['mgmt'] == 'control')]['VELMA_ID'].tolist()
-
-# for i, dist_name in enumerate(dist_names):
-#     filter_map = ~np.isin(cover_ids, no_mgmt_stands) * 1  # 1=disturbance, 0=no disturbance
-#     filter_map[no_mgmt_buffer] = 0
-#     # plt.imshow(filter_map)
-#     outfile = outdir / '{}_filter.asc'.format(dist_name)
-#     flow.raster_header
-#     f = open(outfile, 'w')
-#     f.write(flow.raster_header)
-#     np.savetxt(f, filter_map, fmt='%i')
-#     f.close()
-
-
-# Create a map of stand IDs
-
+map3 = create_filter_map(disturbance, stands_exclude, stands_include)
