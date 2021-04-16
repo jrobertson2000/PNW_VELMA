6ee8623102988762f0bb1a4c36f3144e8c73f535
ianpdavies <yon.davies@gmail.com>
2021-04-16 12:22:50 -0700

    Removed stand id resampling

--- scripts/formatting/other_layers.py
+++ scripts/formatting/other_layers.py
@@ -18,30 +18,6 @@ env.workspace = tmp_dir  # Set to temp so that rasters created during velma_form
 arcpy.env.overwriteOutput = True
 
 # =======================================================================
-# Mosaic rasters
-# =======================================================================
-
-# Mosaic biomass
-try:
-    biomass_list = [str(x) for x in config.biomass_list]
-    biomass_mosaic = 'biomass_mosaic.tif'
-    arcpy.MosaicToNewRaster_management(input_rasters=biomass_list, output_location=tmp_dir,
-                                       raster_dataset_name_with_extension=biomass_mosaic, pixel_type='32_BIT_SIGNED',
-                                       number_of_bands=1)
-except AttributeError:
-    pass
-
-# Mosaic imperv
-try:
-    imperv_list = [str(x) for x in config.imperv_list]
-    imperv_mosaic = 'imperv__mosaic.tif'
-    arcpy.MosaicToNewRaster_management(input_rasters=imperv_list, output_location=tmp_dir,
-                                       raster_dataset_name_with_extension=imperv_mosaic, pixel_type='32_BIT_SIGNED',
-                                       number_of_bands=1)
-except AttributeError:
-    pass
-
-# =======================================================================
 # Clip to study area buffer, reproject, resample rasters
 # =======================================================================
 
@@ -53,27 +29,6 @@ dem_velma = str(config.dem_velma)
 roi_layers = getROI(roi, tmp_dir, proj)
 
 dem_specs = getDEMspecs(dem_velma)
-
-# # Biomass
-# try:
-#     biomass = tmp_dir + '/' + biomass_mosaic
-# except NameError:
-#     biomass = str(config.biomass)
-# reshape(config.biomass, config.biomass_out, 'biomass', 'NEAREST', tmp_dir, roi_layers)
-
-# # Impervious surfaces
-# try:
-#     imperv = tmp_dir + '/' + imperv_mosaic
-# except NameError:
-#     imperv = str(config.imperv)
-# imperv_tmp = tmp_dir / 'imperv.tif'
-# reshape(config.imperv, imperv_tmp, 'imperv', 'NEAREST', dem_specs, cell_size, proj, tmp_dir, roi_layers)
-# velma_format(imperv_tmp, config.imperv_velma)
-
-# Cover ID
-cover_id_tmp = tmp_dir + '/cover_id.tif'
-reshape(config.cover_id, cover_id_tmp, 'cover_id', 'NEAREST', dem_specs, cell_size, proj, tmp_dir, roi_layers)
-velma_format(cover_id_tmp, config.cover_id_velma)
 
 # Cover age
 cover_age_tmp = tmp_dir + '/cover_age.tif'
